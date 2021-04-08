5445665b65103cd2e802e283b4f54f8357b2dc4e
tanvirtin <tanvir.tinz@gmail.com>
2021-04-07 22:51:11 -0400

    add init test

---
+++ tests/init_spec.lua
@@ -0,0 +1,30 @@
+local git = require('git')
+
+local vim = vim
+local it = it
+local describe = describe
+
+describe('init:', function()
+
+      describe('imports', function()
+
+        it('should make sure all necessary functionalities get exposed', function()
+            local known_imports = {
+                setup = true,
+                buf_attach = true,
+                buf_detach = true,
+                hunk_preview = true,
+                hunk_up = true,
+                hunk_down = true,
+                hunk_reset = true,
+                diff = true,
+                files_changed = true
+            }
+            for key, _ in pairs(git) do
+                assert(known_imports[key])
+            end
+        end)
+
+    end)
+
+end)
