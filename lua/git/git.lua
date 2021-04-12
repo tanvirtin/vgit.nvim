15623b8af2c89c92253d13f878479aa4e9532591
tanvirtin <tanvir.tinz@gmail.com>
2021-04-11 23:12:48 -0400

    I am now waitng for the plenary jobs to finish instead of callback nestng

--- lua/git/git.lua
+++ lua/git/git.lua
@@ -74,7 +74,7 @@ M.create_hunk = function(header)
     return hunk
 end
 
-M.buffer_hunks = function(filename, callback)
+M.buffer_hunks = function(filename)
     local err_result = ''
     local hunks = {}
     local job = Job:new({
@@ -107,104 +107,101 @@ M.buffer_hunks = function(filename, callback)
                 err_result = err_result .. line
             end
         end,
-        on_exit = function()
-            if err_result ~= '' then
-                return callback(err_result, nil)
-            end
-            callback(nil, hunks)
-        end,
     })
     job:sync()
-    return job
+    job:wait()
+    if err_result ~= '' then
+        return err_result, nil
+    end
+    return nil, hunks
 end
 
-M.diff = function(filename, hunks, callback)
-    return fs.read_file(filename, vim.schedule_wrap(function(err, data)
-        if err then
-            return callback(err, nil)
-        end
-        local file_type = fs.file_type(filename)
-        data = vim.split(data, '\n')
-        local cwd_lines = {}
-        local origin_lines = {}
-        local lnum_changes = {
-            origin = {
-                added = {},
-                removed = {}
-            },
-            cwd = {
-                added = {},
-                removed = {}
-            },
-        }
-        -- shallow copy
-        for key, value in pairs(data) do
-            cwd_lines[key] = value
-            origin_lines[key] = value
-        end
-        -- Operations below will potentially add more lines to both cwd and
-        -- origin data, which means, the offset needs to be added to our hunks.
-        local new_lines_added = 0
-        for _, hunk in ipairs(hunks) do
-            local type = hunk.type
-            local start = hunk.start + new_lines_added
-            local finish = hunk.finish + new_lines_added
-            local diff = hunk.diff
-            if type == 'add' then
-                -- Remove the line indicating that these lines were inserted in cwd_lines.
-                for i = start, finish do
-                    origin_lines[i] = ''
+M.diff = function(filename, hunks)
+    local err, data = fs.read_file(filename);
+    if err then
+        return err, nil
+    end
+    local file_type = fs.file_type(filename)
+    data = vim.split(data, '\n')
+    local cwd_lines = {}
+    local origin_lines = {}
+    local lnum_changes = {
+        origin = {
+            added = {},
+            removed = {}
+        },
+        cwd = {
+            added = {},
+            removed = {}
+        },
+    }
+    -- shallow copy
+    for key, value in pairs(data) do
+        cwd_lines[key] = value
+        origin_lines[key] = value
+    end
+    -- Operations below will potentially add more lines to both cwd and
+    -- origin data, which means, the offset needs to be added to our hunks.
+    local new_lines_added = 0
+    for _, hunk in ipairs(hunks) do
+        local type = hunk.type
+        local start = hunk.start + new_lines_added
+        local finish = hunk.finish + new_lines_added
+        local diff = hunk.diff
+        if type == 'add' then
+            -- Remove the line indicating that these lines were inserted in cwd_lines.
+            for i = start, finish do
+                origin_lines[i] = ''
+                table.insert(lnum_changes.cwd.added, i)
+            end
+        elseif type == 'remove' then
+            for _, line in ipairs(diff) do
+                start = start + 1
+                new_lines_added = new_lines_added + 1
+                table.insert(cwd_lines, start, '')
+                table.insert(origin_lines, start, line:sub(2, #line))
+                table.insert(lnum_changes.origin.removed, start)
+            end
+        elseif type == 'change' then
+            -- Retrieve lines that have been removed and added without "-" and "+".
+            local removed_lines, added_lines = parse_hunk_diff(diff)
+            -- Max lines are the maximum number of lines found between added and removed lines.
+            local max_lines = 0
+            if #removed_lines > #added_lines then
+                max_lines = #removed_lines
+            else
+                max_lines = #added_lines
+            end
+            -- Hunk finish index does not indicate the total number of lines that may have a diff.
+            -- Which is why I am inserting empty lines into both the cwd and origin data arrays.
+            for i = finish + 1, (start + max_lines) - 1 do
+                new_lines_added = new_lines_added + 1
+                table.insert(cwd_lines, i, '')
+                table.insert(origin_lines, i, '')
+            end
+            -- With the new calculated range I simply loop over and add the removed
+            -- and added lines to their corresponding arrays that contain a buffer lines.
+            for i = start, start + max_lines - 1 do
+                local recalculated_index = (i - start) + 1
+                local added_line = added_lines[recalculated_index]
+                local removed_line = removed_lines[recalculated_index]
+                if removed_line then
+                    table.insert(lnum_changes.origin.removed, i)
+                end
+                if added_line then
                     table.insert(lnum_changes.cwd.added, i)
                 end
-            elseif type == 'remove' then
-                for _, line in ipairs(diff) do
-                    start = start + 1
-                    new_lines_added = new_lines_added + 1
-                    table.insert(cwd_lines, start, '')
-                    table.insert(origin_lines, start, line:sub(2, #line))
-                    table.insert(lnum_changes.origin.removed, start)
-                end
-            elseif type == 'change' then
-                -- Retrieve lines that have been removed and added without "-" and "+".
-                local removed_lines, added_lines = parse_hunk_diff(diff)
-                -- Max lines are the maximum number of lines found between added and removed lines.
-                local max_lines = 0
-                if #removed_lines > #added_lines then
-                    max_lines = #removed_lines
-                else
-                    max_lines = #added_lines
-                end
-                -- Hunk finish index does not indicate the total number of lines that may have a diff.
-                -- Which is why I am inserting empty lines into both the cwd and origin data arrays.
-                for i = finish + 1, (start + max_lines) - 1 do
-                    new_lines_added = new_lines_added + 1
-                    table.insert(cwd_lines, i, '')
-                    table.insert(origin_lines, i, '')
-                end
-                -- With the new calculated range I simply loop over and add the removed
-                -- and added lines to their corresponding arrays that contain a buffer lines.
-                for i = start, start + max_lines - 1 do
-                    local recalculated_index = (i - start) + 1
-                    local added_line = added_lines[recalculated_index]
-                    local removed_line = removed_lines[recalculated_index]
-                    if removed_line then
-                        table.insert(lnum_changes.origin.removed, i)
-                    end
-                    if added_line then
-                        table.insert(lnum_changes.cwd.added, i)
-                    end
-                    origin_lines[i] = removed_line or ''
-                    cwd_lines[i] = added_line or ''
-                end
+                origin_lines[i] = removed_line or ''
+                cwd_lines[i] = added_line or ''
             end
         end
-        callback(nil, {
-            cwd_lines = cwd_lines,
-            origin_lines = origin_lines,
-            lnum_changes = lnum_changes,
-            file_type = file_type
-        })
-    end));
+    end
+    return nil, {
+        cwd_lines = cwd_lines,
+        origin_lines = origin_lines,
+        lnum_changes = lnum_changes,
+        file_type = file_type
+    }
 end
 
 return M
--- tests/fs_spec.lua
+++ tests/fs_spec.lua
@@ -7,27 +7,15 @@ describe('fs:', function()
 
       describe('read_file', function()
 
-        it('should retrieve contents of a file as a string for a given file path in a callback', function()
-            local err_result = nil
-            local data = nil
-            local job = fs.read_file('README.md', function(err, content)
-                err_result = err
-                data = content
-            end)
-            job:wait()
-            assert.are.same(err_result, nil)
+        it('should retrieve contents of a file as a string for a given file path', function()
+            local err, data = fs.read_file('README.md')
+            assert.are.same(err, nil)
             assert.are.same(type(data), 'string')
         end)
 
-        it('should retrieve an err_result for a given file path that does not exists in a callback', function()
-            local err_result = nil
-            local data = nil
-            local job = fs.read_file('IDONTEXIST.md', function(err, content)
-                err_result = err
-                data = content
-            end)
-            job:wait()
-            assert.are_not.same(err_result, nil)
+        it('should retrieve an err_result for a given file path that does not exist', function()
+            local err, data = fs.read_file('IDONTEXIST.md')
+            assert.are_not.same(err, nil)
             assert.are.same(data, nil)
         end)
 
