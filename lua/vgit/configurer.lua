a1f4d7fd69f3ca95f1d4b8ed0161956873423cf9
tanvirtin <tanvir.tinz@gmail.com>
2021-05-12 00:56:59 -0400

    config is done in reverse to make things a bit more optimized

--- lua/vgit/configurer.lua
+++ lua/vgit/configurer.lua
@@ -9,12 +9,12 @@ M.assign = function(state, config)
         local config_segment_type = type(config_segment)
         assert(state_segment_type == config_segment_type, 'invalid config')
         if config_segment_type == 'table' then
-            for key, config_value in pairs(config_segment) do
-                local state_value = state_segment[key]
-                if state_value then
+            for key, state_value in pairs(state_segment) do
+                local config_value = config_segment[key]
+                if config_value ~= nil then
                     local state_value_type = type(state_value)
                     local config_value_type = type(config_value)
-                    if state_value_type == 'table' then
+                    if config_value_type == 'table' then
                         assign(state_segment[key], config_segment[key])
                     else
                         assert(state_value_type == config_value_type, 'invalid config')
