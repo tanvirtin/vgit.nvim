db054d59d8fbe58d93d21c897f8336cd960ed168
tanvirtin <tanvir.tinz@gmail.com>
2021-06-04 16:34:15 -0400

    users can now change diff base on the fly

--- tests/unit/Bstate_spec.lua
+++ tests/unit/Bstate_spec.lua
@@ -22,7 +22,7 @@ describe('Bstate:', function()
 
         it('should create a Bstate object', function()
             local bstate = Bstate.new()
-            eq(bstate, { bufs = {} })
+            eq(bstate, { buf_states = {} })
         end)
 
     end)
@@ -39,7 +39,7 @@ describe('Bstate:', function()
                 current = atomic_buf_state,
                 initial = atomic_buf_state,
             }
-            eq(bstate.bufs, {
+            eq(bstate.buf_states, {
                 ['1'] = buf_state,
                 ['2'] = buf_state,
                 ['3'] = buf_state,
@@ -54,7 +54,7 @@ describe('Bstate:', function()
             for i = 1, num_cache, 1 do
                 bstate:add(i)
             end
-            eq(#vim.tbl_keys(bstate.bufs), num_cache)
+            eq(#vim.tbl_keys(bstate.buf_states), num_cache)
         end)
 
     end)
@@ -81,6 +81,28 @@ describe('Bstate:', function()
             for i = 101, num_bufs, 1 do
                 eq(bstate:contains(i), false)
             end
+        end)
+
+    end)
+
+    describe('get_bufs', function()
+
+        it('should return all the buf states that exists in the bstate', function()
+            local bstate = Bstate.new()
+            local num_bufs = 100
+            for i = 1, num_bufs, 1 do
+                bstate:add(i)
+            end
+            local buf_states = bstate:get_buf_states()
+            eq(#vim.tbl_keys(buf_states), num_bufs)
+            for _, buf_state in pairs(buf_states) do
+                assert(buf_state, initial_state)
+            end
+        end)
+
+        it('should return empty table if there are no buf states in the bstate', function()
+            local bstate = Bstate.new()
+            assert(bstate:get_buf_states(), {})
         end)
 
     end)
