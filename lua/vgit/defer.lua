1baf67e382d39d28ca6d110f3b18b772693034fe
tanvirtin <tanvir.tinz@gmail.com>
2021-06-17 17:24:00 -0400

    remove pcall from throttle_leading

--- lua/vgit/defer.lua
+++ lua/vgit/defer.lua
@@ -5,16 +5,15 @@ local M = {}
 M.throttle_leading = function(fn, ms)
     local timer = vim.loop.new_timer()
     local running = false
-    local function wrapped_fn(...)
+    return function (...)
         if not running then
             timer:start(ms, 0, function()
                 running = false
             end)
             running = true
-            pcall(fn, select(1, ...))
+            fn(...)
         end
     end
-    return wrapped_fn
 end
 
 return M
