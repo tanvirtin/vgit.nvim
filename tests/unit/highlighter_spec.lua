b1881807e7dc04ecf5a07217d87c8dcb322d89f1
tanvirtin <tanvir.tinz@gmail.com>
2021-08-21 18:12:58 -0400

    feat #32

--- tests/unit/highlighter_spec.lua
+++ tests/unit/highlighter_spec.lua
@@ -1,19 +1,19 @@
-local highlighter = require('vgit.highlighter')
+local highlight = require('vgit.highlight')
 
 local it = it
 local describe = describe
 local eq = assert.are.same
 
-describe('highlighter:', function()
+describe('highlight:', function()
     describe('setup', function()
         it('should override state highlights with highlights specified through the config', function()
-            highlighter.setup({
+            highlight.setup({
                 VGitSignAdd = {
                     fg = 'red',
                     bg = nil,
                 },
             })
-            eq(highlighter.state.data.VGitSignAdd, {
+            eq(highlight.state.data.VGitSignAdd, {
                 fg = 'red',
                 bg = nil,
             })
@@ -23,7 +23,7 @@ describe('highlighter:', function()
     describe('create', function()
         it('should successfully create a highlight', function()
             local hl = 'VGitTestHighlight'
-            highlighter.create(hl, {
+            highlight.create(hl, {
                 bg = nil,
                 fg = '#464b59',
             })
