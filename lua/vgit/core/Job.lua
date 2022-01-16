b36b1d8dbe32ca4d49af810b6b2db5e23ca7af90
Tanvir Islam <tanvir.tinz@gmail.com>
2022-01-16 14:58:04 -0500

    feat: Code lens | Authorship (Scroll up or press CTRL-B on top of the file to see)
    Adds Git authorship code lens to the top of the file.
    Recent Change — author and date of the most recent commit for the file.

--- lua/vgit/core/Job.lua
+++ lua/vgit/core/Job.lua
@@ -1,10 +1,14 @@
 local Object = require('vgit.core.Object')
+local loop = require('vgit.core.loop')
+
 local Job = Object:extend()
 
 function Job:new(spec)
-  return setmetatable({
-    spec = spec,
-  }, Job)
+  return setmetatable({ spec = spec }, Job)
+end
+
+function Job:is_background()
+  return self.spec.is_background == true
 end
 
 function Job:parse_result(output, callback)
@@ -13,6 +17,9 @@ function Job:parse_result(output, callback)
   end
   local line = ''
   for i = 1, #output do
+    if self:is_background() and i % 100 == 0 then
+      loop.await_fast_event()
+    end
     local char = output:sub(i, i)
     if char == '\n' then
       callback(line)
@@ -26,6 +33,13 @@ function Job:parse_result(output, callback)
   end
 end
 
+function Job:wrap_callback(callback)
+  if self:is_background() then
+    return vim.schedule_wrap(callback)
+  end
+  return callback
+end
+
 function Job:start()
   local stdout_result = ''
   local stderr_result = ''
@@ -33,11 +47,19 @@ function Job:start()
   local stdout = vim.loop.new_pipe(false)
   local stderr = vim.loop.new_pipe(false)
 
-  vim.loop.spawn(self.spec.command, {
-    args = self.spec.args,
-    stdio = { nil, stdout, stderr },
-    cwd = self.spec.cwd,
-  }, function(code, signal)
+  local on_stdout = function(_, chunk)
+    if chunk then
+      stdout_result = stdout_result .. chunk
+    end
+  end
+
+  local on_stderr = function(_, chunk)
+    if chunk then
+      stderr_result = stderr_result .. chunk
+    end
+  end
+
+  local on_exit = loop.async(function(code, signal)
     stdout:read_stop()
     stderr:read_stop()
 
@@ -56,17 +78,14 @@ function Job:start()
     end
   end)
 
-  stdout:read_start(function(_, chunk)
-    if chunk then
-      stdout_result = stdout_result .. chunk
-    end
-  end)
+  vim.loop.spawn(self.spec.command, {
+    args = self.spec.args,
+    stdio = { nil, stdout, stderr },
+    cwd = self.spec.cwd,
+  }, self:wrap_callback(on_exit))
 
-  stderr:read_start(function(_, chunk)
-    if chunk then
-      stderr_result = stderr_result .. chunk
-    end
-  end)
+  stdout:read_start(self:wrap_callback(on_stdout))
+  stderr:read_start(self:wrap_callback(on_stderr))
 
   return self
 end
