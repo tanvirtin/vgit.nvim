a12e5655e661e99d3595adf93d74807e2774b5d5
tanvirtin <tanvir.tinz@gmail.com>
2022-05-16 23:52:22 -0400

    feat: connect gutter_blame_preview with project_commits_preview (#262)

--- lua/vgit/features/screens/GutterBlameScreen/init.lua
+++ lua/vgit/features/screens/GutterBlameScreen/init.lua
@@ -1,6 +1,7 @@
 local loop = require('vgit.core.loop')
 local Scene = require('vgit.ui.Scene')
 local Feature = require('vgit.Feature')
+local Window = require('vgit.core.Window')
 local Buffer = require('vgit.core.Buffer')
 local console = require('vgit.core.console')
 local CodeView = require('vgit.ui.views.CodeView')
@@ -42,6 +43,30 @@ function GutterBlameScreen:trigger_keypress(key, ...)
   return self
 end
 
+function GutterBlameScreen:open_commit()
+  loop.await_fast_event()
+  local lnum = Window(0):get_lnum()
+  loop.await_fast_event()
+  local err_blames, blames = self.query:get_blames()
+
+  if err_blames then
+    return self
+  end
+
+  local blame = blames[lnum]
+
+  if not blame.committed then
+    return self
+  end
+
+  vim.cmd('quit')
+
+  loop.await_fast_event()
+  vim.cmd(string.format('VGit project_commits_preview %s', blame.commit_hash))
+
+  return self
+end
+
 function GutterBlameScreen:show()
   console.log('Processing blames')
 
@@ -58,8 +83,26 @@ function GutterBlameScreen:show()
   end
 
   loop.await_fast_event()
-  self.gutter_blame_view:show()
-  self.code_view:show(layout_type)
+  self.gutter_blame_view:show():set_keymap({
+    {
+      mode = 'n',
+      key = '<enter>',
+      vgit_key = 'keys.enter',
+      handler = loop.async(function()
+        self:open_commit()
+      end),
+    },
+  })
+  self.code_view:show(layout_type):set_keymap({
+    {
+      mode = 'n',
+      key = '<enter>',
+      vgit_key = 'keys.enter',
+      handler = loop.async(function()
+        self:open_commit()
+      end),
+    },
+  })
 
   return true
 end
