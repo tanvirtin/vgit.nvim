6cc0d6e0e63204d9c07452755b05e66cdffdfb31
tanvirtin <tanvir.tinz@gmail.com>
2023-01-04 08:53:41 +0600

    feat(project_commits_preview): add support for opening files (#304)
    Users are now able to open a file from project_commits_preview when they press enter on either the list or the diff preview.

--- lua/vgit/features/screens/ProjectCommitsScreen/init.lua
+++ lua/vgit/features/screens/ProjectCommitsScreen/init.lua
@@ -1,7 +1,10 @@
+local fs = require('vgit.core.fs')
 local loop = require('vgit.core.loop')
 local Scene = require('vgit.ui.Scene')
 local utils = require('vgit.core.utils')
 local Object = require('vgit.core.Object')
+local Buffer = require('vgit.core.Buffer')
+local Window = require('vgit.core.Window')
 local console = require('vgit.core.console')
 local CodeView = require('vgit.ui.views.CodeView')
 local FSListGenerator = require('vgit.ui.FSListGenerator')
@@ -61,16 +64,83 @@ function ProjectCommitsScreen:get_list_title(commits)
   return utils.object.size(commits) == 1 and utils.object.first(commits):sub(1, 8) or 'Project commits'
 end
 
-function ProjectCommitsScreen:hunk_up()
-  self.code_view:prev()
+function ProjectCommitsScreen:hunk_up() self.code_view:prev() end
 
-  return self
+function ProjectCommitsScreen:hunk_down() self.code_view:next() end
+
+function ProjectCommitsScreen:handle_list_move_down()
+  local list_item = self.foldable_list_view:move('down')
+
+  if not list_item then
+    return
+  end
+
+  self.store:set_id(list_item.id)
+  self.code_view:render_debounced(loop.async(function() self.code_view:navigate_to_mark(1) end))
 end
 
-function ProjectCommitsScreen:hunk_down()
-  self.code_view:next()
+function ProjectCommitsScreen:handle_list_move_up()
+  local list_item = self.foldable_list_view:move('up')
 
-  return self
+  if not list_item then
+    return
+  end
+
+  self.store:set_id(list_item.id)
+  self.code_view:render_debounced(function() self.code_view:navigate_to_mark(1) end)
+end
+
+function ProjectCommitsScreen:handle_on_enter()
+  local _, filename = self.store:get_filename()
+
+  if not filename then
+    self.foldable_list_view:toggle_current_list_item():render()
+
+    return
+  end
+
+  if not fs.exists(filename) then
+    local commit_err, commit_hash = self.store:get_parent_commit()
+
+    if commit_err then
+      console.debug.error(commit_err).error(commit_err)
+
+      return
+    end
+
+    local lines_err, lines = self.store:get_remote_lines(filename, commit_hash)
+    loop.await()
+
+    if lines_err then
+      console.debug.error(lines_err).error(lines_err)
+
+      return
+    end
+
+    self:destroy()
+
+    vim.cmd('enew')
+
+    local buffer = Buffer(0)
+    local filetype = fs.detect_filetype(filename)
+
+    buffer:set_lines(lines)
+    buffer:set_option('ft', filetype)
+
+    return
+  end
+
+  self:destroy()
+
+  fs.open(filename)
+
+  local diff_dto_err, diff_dto = self.store:get_diff_dto()
+
+  if diff_dto_err or not diff_dto then
+    return
+  end
+
+  Window(0):set_lnum(diff_dto.marks[1].top_relative):position_cursor('center')
 end
 
 function ProjectCommitsScreen:show(commits)
@@ -85,40 +155,21 @@ function ProjectCommitsScreen:show(commits)
   loop.await()
   self.code_view:show(self.layout_type)
   self.foldable_list_view:set_title(self:get_list_title(commits)):show()
-
   self.foldable_list_view:set_keymap({
     {
       mode = 'n',
       key = 'j',
-      handler = loop.async(function()
-        local list_item = self.foldable_list_view:move('down')
-
-        if not list_item then
-          return
-        end
-
-        self.store:set_id(list_item.id)
-        self.code_view:render_debounced(loop.async(function() self.code_view:navigate_to_mark(1) end))
-      end),
+      handler = loop.async(function() self:handle_list_move_down() end),
     },
     {
       mode = 'n',
       key = 'k',
-      handler = loop.async(function()
-        local list_item = self.foldable_list_view:move('up')
-
-        if not list_item then
-          return
-        end
-
-        self.store:set_id(list_item.id)
-        self.code_view:render_debounced(function() self.code_view:navigate_to_mark(1) end)
-      end),
+      handler = loop.async(function() self:handle_list_move_up() end),
     },
     {
       mode = 'n',
       key = '<enter>',
-      handler = loop.async(function() self.foldable_list_view:toggle_current_list_item():render() end),
+      handler = loop.async(function() self:handle_on_enter() end),
     },
   })
   return true
