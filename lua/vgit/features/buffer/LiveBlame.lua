0b704cdc458efe74336597b2782d924f75dcb7c1
tanvirtin <tanvir.tinz@gmail.com>
2023-01-28 16:30:16 -0500

    feat(live_gutter,live_blame,authorship_code_lens): increase reactivity

--- lua/vgit/features/buffer/LiveBlame.lua
+++ lua/vgit/features/buffer/LiveBlame.lua
@@ -1,34 +1,23 @@
 local loop = require('vgit.core.loop')
-local event = require('vgit.core.event')
+local Git = require('vgit.git.cli.Git')
 local Object = require('vgit.core.Object')
-local Buffer = require('vgit.core.Buffer')
 local Window = require('vgit.core.Window')
 local console = require('vgit.core.console')
-local GitBuffer = require('vgit.git.GitBuffer')
 local Namespace = require('vgit.core.Namespace')
-local live_blame_setting = require('vgit.settings.live_blame')
+local event_type = require('vgit.core.event_type')
 local git_buffer_store = require('vgit.git.git_buffer_store')
+local live_blame_setting = require('vgit.settings.live_blame')
 
 local LiveBlame = Object:extend()
 
 function LiveBlame:constructor()
   return {
-    name = 'Live Blame',
     id = 1,
-    last_lnum = nil,
+    name = 'Live Blame',
     namespace = Namespace(),
+    last_lnum = nil,
+    git = Git(),
   }
-end
-
-function LiveBlame:register_events()
-  event
-    .on('BufEnter', function() self:desync_all() end)
-    .on('WinEnter', function() self:desync_all() end)
-    .on('CursorHold', function() self:sync() end)
-    .on('CursorMoved', function() self:desync() end)
-    .on('InsertEnter', function() self:desync() end)
-
-  return self
 end
 
 function LiveBlame:display(lnum, buffer, config, blame)
@@ -48,49 +37,40 @@ function LiveBlame:clear(buffer)
   end
 end
 
-LiveBlame.sync = loop.debounced_async(function(self)
+function LiveBlame:reset()
+  git_buffer_store.for_each(function(git_buffer) self:clear(git_buffer) end)
+end
+
+function LiveBlame:render(git_buffer)
   if not live_blame_setting:get('enabled') then
+    return
+  end
+
+  git_buffer = git_buffer or git_buffer_store.current()
+
+  if not git_buffer then
+    return
+  end
+
+  loop.await()
+  local config_err, config = self.git:config()
+
+  if config_err then
+    console.debug.error(config_err)
     return
   end
 
   loop.await()
   local window = Window(0)
   loop.await()
-  local buffer = git_buffer_store.current()
-  local git_buffer = GitBuffer(buffer)
-
-  if not buffer then
-    return
-  end
-  loop.await()
-
-  if buffer:editing() then
-    console.debug.warning(string.format('Buffer %s is being edited right now', buffer.bufnr))
-    return
-  end
-
-  if not buffer:is_valid() then
-    return
-  end
-
-  if not git_buffer:is_tracked() then
-    return
-  end
-
-  loop.await()
   local lnum = window:get_lnum()
 
-  if self.last_lnum and self.last_lnum == lnum then
+  if self.last_lnum == lnum then
     return
   end
 
   loop.await()
-  local blame_err, blame = buffer.git_object:blame_line(lnum)
-
-  loop.await()
-  if not buffer:is_valid() then
-    return self
-  end
+  local blame_err, blame = git_buffer.git_object:blame_line(lnum)
 
   loop.await()
   local new_lnum = window:get_lnum()
@@ -105,68 +85,23 @@ LiveBlame.sync = loop.debounced_async(function(self)
   end
 
   loop.await()
-  local config_err, config = buffer.git_object:config()
 
-  if config_err then
-    console.debug.error(config_err)
-    return
-  end
-
-  if not buffer then
-    return
-  end
-
-  loop.await()
-  self:clear(buffer)
-  self:display(lnum, buffer, config, blame)
+  self:clear(git_buffer)
+  self:display(lnum, git_buffer, config, blame)
   self.last_lnum = lnum
-end, 20)
-
-function LiveBlame:desync(force)
-  loop.await()
-  local window = Window(0)
-  local buffer = git_buffer_store.current()
-
-  if not buffer then
-    return
-  end
-
-  loop.await()
-  buffer = git_buffer_store.get(buffer)
-  loop.await()
-
-  local lnum = window:get_lnum()
-
-  if not force and self.last_lnum and self.last_lnum == lnum then
-    return
-  end
-
-  if not buffer then
-    return
-  end
-
-  self:clear(buffer)
 end
 
-function LiveBlame:desync_all(force)
-  if not live_blame_setting:get('enabled') and not force then
-    return
-  end
+function LiveBlame:register_events()
+  git_buffer_store.attach('attach', function(git_buffer)
+    git_buffer
+      :on(event_type.BufEnter, function() self:reset() end)
+      :on(event_type.WinEnter, function() self:reset() end)
+      :on(event_type.CursorMoved, function() self:reset() end)
+      :on(event_type.InsertEnter, function() self:reset() end)
+      :on(event_type.CursorHold, function() self:render() end)
+  end)
 
-  local buffers = Buffer:list()
-
-  for i = 1, #buffers do
-    local buffer = buffers[i]
-
-    if buffer then
-      self:clear(buffer)
-    end
-  end
-end
-
-function LiveBlame:resync()
-  self:desync_all(true)
-  self:sync()
+  return self
 end
 
 return LiveBlame
