7e923dd22d6b166640abee346ab47608a4d5a160
tanvirtin <tanvir.tinz@gmail.com>
2025-02-06 23:05:14 -0500

    feat(keymapping): replace help bar with popup

--- lua/vgit/features/screens/DiffScreen/init.lua
+++ lua/vgit/features/screens/DiffScreen/init.lua
@@ -6,7 +6,7 @@ local Window = require('vgit.core.Window')
 local Buffer = require('vgit.core.Buffer')
 local console = require('vgit.core.console')
 local DiffView = require('vgit.ui.views.DiffView')
-local KeyHelpBarView = require('vgit.ui.views.KeyHelpBarView')
+local KeyHelpPopup = require('vgit.ui.popups.KeyHelpPopup')
 local Model = require('vgit.features.screens.DiffScreen.Model')
 local diff_preview_setting = require('vgit.settings.diff_preview')
 
@@ -36,7 +36,8 @@ function DiffScreen:create_diff_view(scene, model)
       },
     })
   end
-  return DiffView(scene, props, { row = 1 }, {
+
+  return DiffView(scene, props, {}, {
     elements = {
       header = true,
       footer = false,
@@ -44,26 +45,12 @@ function DiffScreen:create_diff_view(scene, model)
   })
 end
 
-function DiffScreen:create_app_bar_view(scene, model)
-  if model:is_hunk() then return nil end
-  return KeyHelpBarView(scene, {
-    keymaps = function()
-      local keymaps = diff_preview_setting:get('keymaps')
-      if model:is_staged() then
-        return {
-          { 'Unstage', keymaps['buffer_unstage'] },
-          { 'Unstage hunk', keymaps['buffer_hunk_unstage'] },
-          { 'Switch to Staged View', keymaps['toggle_view'] },
-        }
-      end
-      return {
-        { 'Stage', keymaps['buffer_stage'] },
-        { 'Stage hunk', keymaps['buffer_hunk_stage'] },
-        { 'Reset', keymaps['reset'] },
-        { 'Switch to Unstage View', keymaps['toggle_view'] },
-      }
-    end,
-  })
+function DiffScreen:help()
+  KeyHelpPopup({
+    config = {
+      keymaps = diff_preview_setting:get('keymaps')
+    }
+  }):mount()
 end
 
 function DiffScreen:constructor(opts)
@@ -75,7 +62,6 @@ function DiffScreen:constructor(opts)
     scene = scene,
     model = model,
     diff_view = DiffScreen:create_diff_view(scene, model),
-    app_bar_view = DiffScreen:create_app_bar_view(scene, model),
   }
 end
 
@@ -105,7 +91,6 @@ function DiffScreen:toggle_view(buffer)
 
     self.diff_view:render()
     self.diff_view:move_to_hunk(1, 'center')
-    if self.app_bar_view then self.app_bar_view:render() end
 
     return
   end
@@ -120,7 +105,6 @@ function DiffScreen:toggle_view(buffer)
 
   self.diff_view:render()
   self.diff_view:move_to_hunk(1, 'center')
-  if self.app_bar_view then self.app_bar_view:render() end
 end
 
 function DiffScreen:reset(buffer)
@@ -345,11 +329,6 @@ function DiffScreen:create(opts)
     return false
   end
 
-  if self.app_bar_view then
-    self.app_bar_view:define()
-    self.app_bar_view:mount()
-  end
-
   self.diff_view:define()
   self.diff_view:mount()
   self.diff_view:render()
@@ -358,8 +337,6 @@ function DiffScreen:create(opts)
 
   local mark_index = self.diff_view:get_relative_mark_index(lnum)
   self.diff_view:move_to_hunk(mark_index, 'center')
-
-  if self.app_bar_view then self.app_bar_view:render() end
 
   return true
 end
